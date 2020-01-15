26e75c0490a0a741904e981dac6d8f2d6525c8a0
bmunguia <bmunguia10@gmail.com>
2020-01-14 16:36:12 -0800

    Bug fix

--- SU2_CFD/src/solver_structure.cpp
+++ SU2_CFD/src/solver_structure.cpp
@@ -6125,8 +6125,8 @@ void CSolver::CorrectBoundAnisoHess(CGeometry *geometry, CConfig *config) {
                   for(iMetr = 0; iMetr < nMetr; iMetr++) {
                     hess[i+iMetr] = 0.0;
                   }
+                  dist = 0.0;
                 }
-                dist = 0.0;
                 for(iDim = 0; iDim < nDim; iDim++) {
                   const su2double coord0 = geometry->node[iPoint]->GetCoord(iDim);
                   const su2double coord1 = geometry->node[jPoint]->GetCoord(iDim);
